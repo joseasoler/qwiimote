784702b84ab68c8ef7c22b76de7ff5ec8cb97894
terkhen <terkhen@localhost>
2009-10-27 15:52:23 +0000

    [Fix r10] Removed unneeded define.

--- qwiimote_debug.h
+++ qwiimote_debug.h
@@ -4,10 +4,4 @@
 /* #define QT_NO_DEBUG_OUTPUT   */
 /* #define QT_NO_WARNING_OUTPUT */
 
-    #ifndef QT_NO_DEBUG_OUTPUT
-        #define QW_ASSERT(boolean) Q_ASSERT(boolean)
-    #else
-        #define QW_ASSERT(boolean)
-    #endif
-
 #endif // QWIIMOTE_DEBUG_H
