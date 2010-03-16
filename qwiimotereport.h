7f21a4dd85f41bf900ac8588a4ab9c6960e105ed
Terkhen <Terkhen@localhost>
2010-03-16 19:35:30 +0000

    -Documentation: Add missing Doxygen comments to qwiimotereport.h.

--- qwiimotereport.h
+++ qwiimotereport.h
@@ -10,6 +10,12 @@
 #include <QByteArray>
 #include <QTime>
 
+/**
+  * Stores a received Wiimote report until it is processed.
+  * @see #QIOWiimote.
+  *
+  * @todo time is still unused. Should be used at MotionPlus and to reduce noise at acceleration.
+  */
 struct QWiimoteReport {
     QTime      time; ///< Time of arrival of the report.
     QByteArray data; ///< Data of the report.
