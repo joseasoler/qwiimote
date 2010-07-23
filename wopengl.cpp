86ef27008de1dc4ba049be26b5b4d0844b550111
Terkhen <terkhen@gmail.com>
2010-07-23 10:27:34 +0200

    -Add: Initial code for showing the wiimote.

--- wopengl.cpp
+++ wopengl.cpp
@@ -1,7 +1,6 @@
 #include "wopengl.h"
 
-WOpenGL::WOpenGL(QWidget *parent) :
-    QGLWidget(parent)
+WOpenGL::WOpenGL(QWidget *parent) : QGLWidget(parent)
 {
 }
 
@@ -9,17 +8,111 @@ void WOpenGL::initializeGL()
 {
 	qglClearColor(QColor(0, 0, 0));
 
+	glShadeModel(GL_FLAT);
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
-	glShadeModel(GL_SMOOTH);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
 }
 
 void WOpenGL::paintGL()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
+	glTranslatef(0.0, 0.0, -10.0);
+	glRotatef( 0, 1.0, 0.0, 0.0);
+	glRotatef(45, 0.0, 1.0, 0.0);
+	glRotatef(45, 0.0, 0.0, 1.0);
+
+	float size = 0.1;
+	float vertex[8][3] = {
+		{ size,  size * 0.75,  size * 4},
+		{-size,  size * 0.75,  size * 4},
+		{-size,  size * 0.75, -size * 4},
+		{ size,  size * 0.75, -size * 4},
+		{ size, -size * 0.75,  size * 4},
+		{-size, -size * 0.75,  size * 4},
+		{-size, -size * 0.75, -size * 4},
+		{ size, -size * 0.75, -size * 4}
+	};
+
+	/* Draw the faces. */
+	glPolygonMode(GL_FRONT, GL_FILL);
+	glBegin(GL_QUADS);
+		glColor3f(1.0, 1.0, 1.0);
+
+		glVertex3fv(vertex[0]);
+		glVertex3fv(vertex[1]);
+		glVertex3fv(vertex[2]);
+		glVertex3fv(vertex[3]);
+
+		glVertex3fv(vertex[4]);
+		glVertex3fv(vertex[5]);
+		glVertex3fv(vertex[1]);
+		glVertex3fv(vertex[0]);
+
+		glVertex3fv(vertex[7]);
+		glVertex3fv(vertex[4]);
+		glVertex3fv(vertex[0]);
+		glVertex3fv(vertex[3]);
+
+		glVertex3fv(vertex[6]);
+		glVertex3fv(vertex[7]);
+		glVertex3fv(vertex[3]);
+		glVertex3fv(vertex[2]);
+
+		glVertex3fv(vertex[5]);
+		glVertex3fv(vertex[6]);
+		glVertex3fv(vertex[2]);
+		glVertex3fv(vertex[1]);
+
+		glVertex3fv(vertex[7]);
+		glVertex3fv(vertex[6]);
+		glVertex3fv(vertex[5]);
+		glVertex3fv(vertex[4]);
+
+	glEnd();
+
+	for (int i = 0; i < 8; i++) {
+		vertex[i][0] *= 1.001;
+		vertex[i][1] *= 1.001;
+		vertex[i][2] *= 1.001;
+	}
+
+	/* Draw the edges. */
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glBegin(GL_QUADS);
+		glColor3f(0.0, 0.0, 0.0);
+
+		glVertex3fv(vertex[0]);
+		glVertex3fv(vertex[1]);
+		glVertex3fv(vertex[2]);
+		glVertex3fv(vertex[3]);
+
+		glVertex3fv(vertex[4]);
+		glVertex3fv(vertex[5]);
+		glVertex3fv(vertex[1]);
+		glVertex3fv(vertex[0]);
+
+		glVertex3fv(vertex[7]);
+		glVertex3fv(vertex[4]);
+		glVertex3fv(vertex[0]);
+		glVertex3fv(vertex[3]);
+
+		glVertex3fv(vertex[6]);
+		glVertex3fv(vertex[7]);
+		glVertex3fv(vertex[3]);
+		glVertex3fv(vertex[2]);
+
+		glVertex3fv(vertex[5]);
+		glVertex3fv(vertex[6]);
+		glVertex3fv(vertex[2]);
+		glVertex3fv(vertex[1]);
+
+		glVertex3fv(vertex[7]);
+		glVertex3fv(vertex[6]);
+		glVertex3fv(vertex[5]);
+		glVertex3fv(vertex[4]);
+
+	glEnd();
 }
 
 void WOpenGL::resizeGL(int width, int height)
@@ -29,6 +122,6 @@ void WOpenGL::resizeGL(int width, int height)
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(-0.5, +0.5, -0.5, +0.5, 4.0, 15.0);
+	glOrtho(-0.5, +0.5, +0.5, -0.5, 4.0, 15.0);
 	glMatrixMode(GL_MODELVIEW);
 }
